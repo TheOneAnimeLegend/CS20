a8b533b84092fb579602edd8e939aaeffd150e74
MercersKitchen <Mark.Mercer@epsb.ca>
2018-09-24 17:22:02 -0600

    Update

--- Websites/JavaScript Lessons/Swapping Variables/Function Arrays Notes/index.js
+++ Websites/JavaScript Lessons/Swapping Variables/Function Arrays Notes/index.js
@@ -12,6 +12,8 @@ let MarkMercer = ["Mark", "Mercer", 13, 29, null, true, false];
 let MeganSpence = ["Megan", "Spence", 14, null, true, false, true];
 
  //Calling functions, with arguements
-greet (MarkMercer[0], MarkMercer[1]);
-greet (MeganSpence[0], MeganSpence[1]);
-greet (square(MarkMercer[2]), MeganSpence[2]);
+greet (MarkMercer[0], MarkMercer[1]); //Simple argument passing
+greet (MeganSpence[0], MeganSpence[1]); //Simple arguement passing
+greet (square(MarkMercer[2]), MeganSpence[2]); //Passing to a Function Return, then to other function
+
+// Last GREET illustrates that the variables in the function are only place holders for what is passed to it
