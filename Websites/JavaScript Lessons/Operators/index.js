d8964bcff0fad0fa61f98e901019ae60d56ef067
MercersKitchen <Mark.Mercer@epsb.ca>
2018-09-26 13:40:11 -0600

    Update

---
+++ Websites/JavaScript Lessons/Operators/index.js
@@ -0,0 +1,16 @@
+function simpleOperators (numberA, numberB){
+  let adding = numberA + numberB;
+  let subtract = numberA - numberB;
+  let mutliply = numberA * numberB;
+  let divide = numberA / numberB;
+  console.log(adding);
+  console.log(subtract);
+  console.log(mutliply);
+  console.log(divide);
+}
+
+let number = [4, 7];
+simpleOperators (number[0], number[1]);
+
+
+let skipCount
