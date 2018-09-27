5390b6d1355e0899f0619ad809273cf49eb54800
MercersKitchen <Mark.Mercer@epsb.ca>
2018-09-27 13:15:05 -0600

    Update

--- Websites/JavaScript Lessons/Operators/index.js
+++ Websites/JavaScript Lessons/Operators/index.js
@@ -8,14 +8,15 @@ function simpleOperators (numberA, numberB){
   console.log(mutliply);
   console.log(divide);
 }
-
+//Skipcount by ___ for ___ times starting at ___
+function skipCounting (skipCount, times, startingNumber){
+  let number = startingNumber - skipCount;
+  for (let i=1; i<=times; i++){
+    number += skipCount;
+    console.log(number);
+  }
+}
 let number = [4, 7, 13, 99];
 simpleOperators (number[0], number[1]);
 simpleOperators (number[2], number[3]);
-
-let skipCount = 15;
-console.log(skipCount);
-for (let i=1; i<=5; i++){
-  skipCount *= 3;
-  console.log(skipCount);
-}
+skipCounting (3, 6, 113);
