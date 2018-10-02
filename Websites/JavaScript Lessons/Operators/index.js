6dc3fe89525cfe67122dde2315e1ffc1278224bf
MercersKitchen <Mark.Mercer@epsb.ca>
2018-10-02 14:47:14 -0600

    Update

--- Websites/JavaScript Lessons/Operators/index.js
+++ Websites/JavaScript Lessons/Operators/index.js
@@ -33,25 +33,16 @@ function StrictEquality (firstOperand, secondOperant) {
   console.log(firstOperand === secondOperant);
 }
 
-//function notEqual(valueArray, i) { //When to stop in an array data set
-//  if (valueArray !=5) { //This is when we do stop, so we accept every other value (common logic for "not")
-//    console.log(number[i], "Is index number: ", i, "with a total array index of ", number.length);
-//    return i;
-//  } else {console.log("5 is at the index", i); i=number.length} //once we stop, we want to jump out of the loop
+function preferences (colour){
+  let defaultColor = 'blue'; //Default colour chosen by developer
+  return currentColor = colour || defaultColor;
+}
 
-//}
-
-//Not Equality Starter Code
-//Outputing individual values of an array, we want to start stop at the value 5, where ever it is in the array
-//for (let i=0; i<number.length; i++) { //we do not know when to stop in the array
-
-//}
+function customerType (points){
+  return type = points > 100 ? 'gold' : 'silver' //points > 100 returns a Boolean; when true assigns string of gold, otherwise silver
+}
 
 let number = [4, 7, 13, 99, 0, 100, 5, 1, '1']; //able to add any typeof to an array, index is automatic
-//for(let i=0; i==done; i++) {
-//  notEqual (if i == number[i], i );
-//}
-
 
 //simpleOperators (number[0], number[1]);
 //simpleOperators (number[2], number[3]);
@@ -66,17 +57,23 @@ let number = [4, 7, 13, 99, 0, 100, 5, 1, '1']; //able to add any typeof to an a
 //StrictEquality (number[8], number[7]); // Returns False, compares typeof variable and value
 
 
-//Introduces Break and Continue Code
-//See: https://github.com/QEHS-Websites/JavaScript-Sandbox/tree/master/Control%20Flow#break-and-continue-key-words-and-loop-concepts
+//Not Equality Starter Code, no function defined here yet
+//Computer first uses TRUE to evaluate ideas, "not" provides an answer of TRUE
+//Outputing individual values of an array, we want to  stop at the value 5, but we do not know where it is in the array (do not know the index value)
+//Must Read the Array with a FOR Loop
+/*
+for (let i=0; i<number.length; i++) { //we do not know when to stop in the array
+  if (valueArray !=5) { //This is when we do stop, so we accept every other value (common logic for "not")
+    console.log(number[i], "Is index number: ", i, "with a total array index of ", number.length);
+  } else {console.log("5 is at the index", i); i=number.length} //once we stop, we want to jump out of the loop
+}
+*/
 
+//Using falsy, for example, preferences
+//console.log(preferences(undefined)); //returns colour passed if an actual colour is passed, for example, 'red'
 
-//Using falsy: preferences
-//let userColor = 'red'; //User not defined yet
-//let defaultColor = 'blue'; //Default colour chosen by developer
-let currentColor = userColor || defaultColor;
-//console.log(currentColor); //returns "blue
-
+//Using Tertiary Operator
 //If a customer has more than 100 points, they are gold, otherwise silver
-let points = 110;
-let type = points > 100 ? 'gold' : 'silver' //points > 100 returns a Boolean; when true assigns string of gold, otherwise silver
-console.log(type); //Returns "silver" in console since false
+console.log(customerType (90));
+
+//Not finished is bitwise example
